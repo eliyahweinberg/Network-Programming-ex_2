60c607d609485fcb9954b2ea60afa0e21d799c4d
Eliyah Weinberg <hebrewman@gmail.com>
2017-12-11 10:52:58 +0200

    Initial Commit

---
+++ ex_2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ex_2
+//
+//  Created by Eliyah Weinberg on 11.12.2017.
+//  Copyright © 2017 Eliyah Weinberg. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
